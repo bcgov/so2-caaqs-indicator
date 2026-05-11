8e81e80847dfba180f0470cef9a27c7880ae09fa
szy-al <Zhuoyan.Song@gov.bc.ca>
2026-05-11 14:36:50 -0700

    Add invisible axis ticks to improving space of y-axis labels

--- 04_output.R
+++ 04_output.R
@@ -236,7 +236,10 @@ g <- ggplot(data = filter(so2_results, !is.na(airzone)), aes(x = metric, fill = 
   theme_soe_facet() +
   theme(panel.grid.major.y = (element_blank()),
         axis.text = element_text(size = 14),
+        axis.text.y = element_text(hjust  = 1),
         axis.title = element_text(size = 14),
+        axis.ticks.y = element_line(colour = "transparent"),
+        axis.ticks.length.y = unit(4, "pt"),
         legend.position = "bottom",
         legend.direction = "vertical",
         legend.box.just = "left",
